b371b1699459ba22363157ac85baf9eb33a964b0
Pulsarfox <samy.vincentffs@gmail.com>
2018-06-04 10:28:58 +0200

    multiple tests

--- main.js
+++ main.js
@@ -2,53 +2,119 @@ const postal = require('postal');
 const N3 = require('n3');
 const { DataFactory } = N3;
 const { namedNode, literal, defaultGraph, quad } = DataFactory;
+const $rdf = require('rdflib');
 
+var RDF = $rdf.Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+var RDFS = $rdf.Namespace("http://www.w3.org/2000/01/rdf-schema#")
+var FOAF = $rdf.Namespace("http://xmlns.com/foaf/0.1/")
+
+/*
+** Initialise postal dans les classes des webcomponent
+*/
 let card = document.querySelector('card-creator');
 let reader = document.querySelector('resource-reader');
 let sparql = document.querySelector('sparql-request');
 let connect = document.querySelector('connect-interface');
-
-
+let aclEditor = document.querySelector('acl-editor');
 card.setPostal(postal);
 reader.setPostal(postal);
 sparql.setPostal(postal);
 connect.setPostal(postal);
+aclEditor.setPostal(postal);
 
-
+/*
+** L'uri du serveur solid sans authentification.
+** Avec authentification elle se presente sous la forme https://username.localhost:8443/
+*/
 let uri = "https://localhost:8443/";
 
+/*
+** Fonctions pour poster une ressource, lire une ressource (par requete normale ou SPARQL)
+** et editer une ressource par requete SPARQL.
+** Utilise fetch()
+*/
 let postCard = postal.subscribe({
     channel:'solid',
     topic:'post-card',
     callback: (data, enveloppe) => {
         let resource = proceedDataToJsonLdCard(data);
-        
-        fetch(uri, {
-            method: 'POST',
-            headers:{
-                'Content-type':'text/turtle',
-                'Link':'<http://www.w3.org/ns/ldp#Resource>; rel="type"',
-                'Slug': 'card'
-            },
-            body: resource
-        }).then(res => {
-            return res.headers.get('Location');
-        })
-        .then(path => {
-            if (path){
-                postal.publish({
-                    channel:'solid',
-                    topic:'done-card',
-                    data:path
-                });
-                postCard.unsubscribe();
-            }
-        })
-        .catch(err => {
-            console.log('err :', err);
-        })
+        if (resource != null){
+            fetch(uri, {
+                method: 'POST',
+                headers:{
+                    'Content-type':'text/turtle',
+                    'Link':'<http://www.w3.org/ns/ldp#Resource>; rel="type"',
+                    'Slug': 'card'
+                },
+                body: resource
+            }).then(res => {
+                return res.headers.get('Location');
+            })
+            .then(path => {
+                if (path){
+                    postal.publish({
+                        channel:'solid',
+                        topic:'done-card',
+                        data:path
+                    });
+                    postCard.unsubscribe();
+                }
+            })
+            .catch(err => {
+                console.log('err :', err);
+            })
+        }
     }
 });
+
+function proceedDataToJsonLdCard(data){
+    let keys = Object.keys(data);
+    let resource = null;
+    let writer = N3.Writer({ prefixes: { 
+        foaf: 'http://xmlns.com/foaf/0.1/',
+        rdf:'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
+    }});
+    writer.addQuad(
+        namedNode('https://savincen.localhost:5000/card'),
+        namedNode('rdf:Type'),
+        namedNode('foaf:PersonalProfileDocument')
+    );
+    writer.addQuad(
+        namedNode('https://savincen.localhost:5000/card'),
+        namedNode('foaf:primaryTopic'),
+        namedNode('#me')
+    );
+    writer.addQuad(
+        namedNode('#me'),
+        namedNode('rdf:Type'),
+        namedNode('foaf:Person')
+    );
+
+    if (keys.length !== 0){
+        let count = 0;
+        for (let key in data){
+            if (data.hasOwnProperty(key) && data[key] != "") {
+                writer.addQuad(
+                    namedNode('#me'),
+                    namedNode('foaf:'+key),
+                    literal(data[key])
+                );
+                count++;
+            }
+        }
+        if (count != 0){
+            writer.end((error, result) => {
+                if (error == null)
+                    resource = result;
+                else 
+                    console.log("writer err, ", error);
+            });
+            return resource;
+        }
+        console.log('resource :', resource);
+    }
+    return null;
+}
 
 let getResource = postal.subscribe({
     channel:'solid',
@@ -92,48 +158,138 @@ let getResource = postal.subscribe({
     }
 });
 
+function proceedDataToSparql(data, card){
+    let req = "";
+    let me = $rdf.sym("https://savincen.localhost:8443/profile/card#me");
+    let store = $rdf.graph();
+    let deleteData = 'DELETE WHERE { \n?s ';
+    //let insertData = 'INSERT DATA { \n?s ';
+    let count = 0;
+    let test = data;
+    let last = Object.keys(test).pop();
+    
+    
+    for(let key in data){
+        if(data.hasOwnProperty(key) && data[key] != ""){
+            deleteData += `${FOAF(key)} ?o `;
+            if (key == last)
+                deleteData += '.\n';
+            else
+                deleteData += ';\n'
+            // insertData += `${FOAF(key)} ${data[key]}`;
+            // if (key == last)
+            //     insertData += '.\n';
+            // else
+            //     insertData += ';\n'
+            count++;
+        }
+        
+    }
+    if (count == 0)
+        return null;
+    deleteData += '}'
+    //insertData += '}';
+    req += deleteData + '\n';
+    //req += insertData;
+    return req;
+}
+
 let sparqlUpdate = postal.subscribe({
     channel:'solid',
     topic:'patch-sparql',
     callback: (data, enveloppe) => {
-        console.log("patch = ", data.request);
+        let card = null;
+        fetch(uri + 'card.ttl', {
+            method:'GET',
+            headers:{'Content-type':'text/turtle'}
+        })
+        .then(res => res.text())
+        .then(text => {
+            card = text;
+        })
+        let request = proceedDataToSparql(data, card);
+        console.log(request);
         
-        fetch(uri + data.name, {
-            method:'PATCH',
-            headers:{'Content-type':'application/sparql-update'},
-            body:data.request
-        })
-        .then(res => {
-            if(res.status >= 400 && res.status < 600){
-                res.text().then(body => {
-                    postal.publish({
-                        channel:'solid',
-                        topic:'err-sparql',
-                        data:body
-                    });
-                })
-            } else {
-                res.text().then(body => {
-                    postal.publish({
-                        channel:'solid',
-                        topic:'done-sparql',
-                        data:body
-                    });
-                })
-            }
-        })
-        .catch(err => {
-            postal.publish({
-                channel:'solid',
-                topic:'err-sparql',
-                data:err
-            });
-        })
+        if (request != null){
+            fetch(uri + 'card.ttl', {
+                method:'PATCH',
+                headers:{'Content-type':'application/sparql-update'},
+                body:request
+            })
+            .then(res => {
+                if(res.status >= 400 && res.status < 600){
+                    res.text().then(body => {
+                        postal.publish({
+                            channel:'solid',
+                            topic:'err-sparql',
+                            data:body
+                        });
+                    })
+                } else {
+                    res.text().then(body => {
+                        postal.publish({
+                            channel:'solid',
+                            topic:'done-sparql',
+                            data:body
+                        });
+                    })
+                }
+            })
+            .catch(err => {
+                postal.publish({
+                    channel:'solid',
+                    topic:'err-sparql',
+                    data:err
+                });
+            })
+        }
     }
 });
 
-var tmpUrl = uri;
+/*
+** Initialise l'OIDCWebClient et verifie a chaque chargement du dom si une session existe ou non
+** si une session existe, remplace fetch par session.fetch, et l'uri par https://username.localhost:8443/
+** sinon, remet l'uri normale et l'api fetch native
+*/
+var OIDCWebClient = OIDC.OIDCWebClient;
+var options = { solid: true };
+var auth = new OIDCWebClient(options);
 
+
+var fetch;
+var channel;
+var logoutBlock = connect.shadowRoot.getElementById("logout");
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    auth.currentSession()
+    .then(session => {
+        console.log('session: ', session);
+        if (!session.hasCredentials()){
+            fetch = window.fetch;
+            uri = "https://localhost:8443/";
+            if (channel != undefined){
+                channel.unsubscribe();
+            }
+            logoutBlock.style.display = "none";
+        } else{
+            let regexp = /(https:\/\/)(.*)(\.localhost.*)/g;
+            let match = regexp.exec(session.idClaims.sub);
+            if(match[2] != null && match[2] != undefined){
+                fetch = session.fetch;
+                uri = `https://${match[2]}.localhost:8443/`;
+                channel = setLogoutChannel();
+                logoutBlock.style.display = "block";            
+            }
+        }
+    })
+});
+
+/*
+** Fonctions de register et login, utilise l'endpoint solidserver/api/accounts/new pour creer un compte
+** et OIDCWebClient.login() pour le login
+*/
+var tmpUrl = uri;
 let register = postal.subscribe({
     channel:'auth',
     topic:'register',
@@ -155,7 +311,7 @@ let register = postal.subscribe({
         })
         .catch(err => console.log('err :', err));
     }
-})
+});
 
 let login = postal.subscribe({
     channel: 'auth',
@@ -168,89 +324,69 @@ let login = postal.subscribe({
             console.log('Empty session, redirecting to login');
             auth.login('https://localhost:8443');
           } else {
-            console.log('hasCredentials() === true');
+            console.log('Already connected');
           } 
         });
     }
-})
+});
 
-function proceedDataToJsonLdCard(data){
-    let keys = Object.keys(data);
-    let resource = "";
-    let writer = N3.Writer({ prefixes: { 
-        foaf: 'http://xmlns.com/foaf/0.1/',
-        rdf:'http://www.w3.org/1999/02/22-rdf-syntax-ns#'
-    }});
-    writer.addQuad(
-        namedNode('https://savincen.localhost:5000/card'),
-        namedNode('rdf:Type'),
-        namedNode('foaf:PersonalProfileDocument')
-    );
-    writer.addQuad(
-        namedNode('https://savincen.localhost:5000/card'),
-        namedNode('foaf:primaryTopic'),
-        namedNode('#me')
-    );
-    writer.addQuad(
-        namedNode('#me'),
-        namedNode('rdf:Type'),
-        namedNode('foaf:Person')
-    );
+function logout(){
+    auth.logout();
 
-    if (keys.length !== 0){
-        for (let key in data){
-            if (data.hasOwnProperty(key)) {
-                let rdfKey;
-                writer.addQuad(
-                    namedNode('#me'),
-                    namedNode('foaf:'+rdfKey),
-                    literal(data[key])
-                );
-            }
+    window.location = "https://savincen.localhost:8443/logout";
+    auth.currentSession()
+    .then(session => {
+        if (channel != undefined){
+            channel.unsubscribe();
         }
-        writer.end((error, result) => {
-            if (error == null)
-                resource = result;
-            else 
-                console.log("writer err, ", error);
-        });
-        console.log('resource :', resource);
-        return resource;
-    }
+        fetch = window.fetch;
+        logoutBlock.style.display = "none";
+        console.log('sessionclosed: ', session);
+    })
+}
+
+function setLogoutChannel(){
+    let ret;
+
+    ret = postal.subscribe({
+        channel:'auth',
+        topic:'logout',
+        callback:logout
+    })
+    return ret;
 }
 
 
-/*--------------AUTHENTICATION--------------*/
+/*
+** Fonctions de manipulation d'ACL
+*/
 
-var OIDCWebClient = OIDC.OIDCWebClient;
-var options = { solid: true };
-var auth = new OIDCWebClient(options);
-
-var fetch; //override fetch method
-
-document.addEventListener('DOMContentLoaded', () => {
-
-    auth.currentSession()
-    .then(session => {
-        console.log('session: ', session);
-        if (!session.hasCredentials()){
-            fetch = window.fetch;
-            uri = "https://localhost:8443/";
-        } else{
-            let regexp = /(https:\/\/)(.*)(\.localhost.*)/g;
-            let match = regexp.exec(session.idClaims.sub);
-            if(match[2] != null && match[2] != undefined){
-                fetch = session.fetch;
-                uri = `https://${match[2]}.localhost:8443/`;
+let exists = postal.subscribe({
+    channel:'wac',
+    topic:'modif-acl',
+    callback: (data, enveloppe) => {
+        let ret = [];
+        ret["status"] == "error";
+        fetch(uri + data.name, {
+            method:'HEAD',
+        })
+        .then(res => {
+            if(res.status == 200){
+                return res.headers.get('Link');
             }
-        }
-    })
-});
-
-document.getElementById("logout").addEventListener('click', e => {
-    auth.logout();
-    auth.currentSession()
-    .then(session => {
-        console.log('sessionclosed: ', session);
-    })
-});
+        })
+        .then(link => {
+            if(link != undefined){
+                let reg = /(.*<)(.*\.acl)(.*)/g;
+                console.log('acl : ', link);
+                let match = reg.exec(link);
+                if (match[2] != null && match[2] != undefined){
+                    console.log(match[2]);
+                }
+            }
+        })
+        .catch(err =>{
+            console.log('err :', err);
+        })
+    }
+})
